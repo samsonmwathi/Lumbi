6df96b617225eb2790d146e1f86b489e442c8517
samsonmwathi <77440440+samsonmwathi@users.noreply.github.com>
2021-08-10 22:47:58 +0300

    Sprint 1 : User Registration
    User's can Register and Login.

--- app/src/main/java/com/example/lumbi/MainActivity.java
+++ app/src/main/java/com/example/lumbi/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.lumbi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
+
+        Button signup = findViewById(R.id.signUpButton);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
