5d253e15bda0a7562615d23433dc398a22b38add
samsonmwathi <77440440+samsonmwathi@users.noreply.github.com>
2021-08-14 11:44:26 +0300

    Rollback Stable
    Current build id stable with user login and signup pages based on the previous rollback

--- app/src/main/java/com/example/lumbi/MainActivity.java
+++ app/src/main/java/com/example/lumbi/MainActivity.java
@@ -1,50 +1,16 @@
 package com.example.lumbi;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navbar);
-        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new homeFragment()).commit();
-        navigationView.setSelectedItemId(R.id.bottom_navbar);
-
-                navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-
-
-                switch (item.getItemId()) {
-                    case R.id.nav_home:
-                        fragment = new homeFragment();
-                        break;
-                    case R.id.nav_settings:
-                        fragment = new settingsFragment();
-                        break;
-                    case R.id.nav_account:
-                        fragment = new searchFragment();
-                        break;
-
-                }
-                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
-                return true;
-            }
-        });
 
     }
 
--- app/src/main/java/com/example/lumbi/auth.java
+++
@@ -1,4 +0,0 @@
-package com.example.lumbi;
-
-public class auth {
-}
