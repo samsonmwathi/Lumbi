465344538c1a3b3d3df31a5ed2c32073826e622f
samsonmwathi <77440440+samsonmwathi@users.noreply.github.com>
2021-08-29 22:27:54 +0300

    Lumbi version 1.5: Getting Started
    Addition of the Getting Started screen for first time user interaction

--- app/src/main/java/com/example/lumbi/SignupActivity.java
+++ app/src/main/java/com/example/lumbi/SignupActivity.java
@@ -129,7 +129,7 @@ public class SignupActivity extends AppCompatActivity {
         startActivity(intent);
     }
     private void SendUserToNextActivity() {
-        Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+        Intent intent = new Intent(SignupActivity.this, GetStartedActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
